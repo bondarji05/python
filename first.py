7d92e7c65ccec3c2b07ea243089714f8aea01c04
bonji05 <bondarji@gmail.com>
2024-02-05 14:52:56 +0300

    final

--- first.py
+++ first.py
@@ -1,13 +1,6 @@
 a = int(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
 c = int(input('Enter 3rd number: '))
-wow = [a, b, c]
-max = max(wow)
-min = min(wow)
-if a > min and a < max:
-    print (a)
-elif b > min and b < max:
-    print(b)
-elif c > min and c < max:
-    print(c)    
-
+arr = [a, b, c]
+arr.sort()
+print(arr[1])
