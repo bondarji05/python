3fed292099e07a6bd66eeca00334d9ed74b742b1
bonji05 <bondarji@gmail.com>
2024-02-05 13:41:06 +0300

    первый вариант

--- first.py
+++ first.py
@@ -1,1 +1,13 @@
-print("hello")
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+c = int(input('Enter 3rd number: '))
+wow = [a, b, c]
+max = max(wow)
+min = min(wow)
+if a > min and a < max:
+    print (a)
+elif b > min and b < max:
+    print(b)
+elif c > min and c < max:
+    print(c)    
+
